eba09cd40d8cd7846591683ce6cfb65a85df435e
SambaJalloh <sambajalloh49gmail.com>
2025-09-07 11:42:30 +0000

    Wk-5 Assignment

---
+++ answers.sql
@@ -0,0 +1,4 @@
+DROP INDEX IdxPhone ON customers;
+CREATE USER 'Jay'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+GRANT INSERT ON salesDB.* TO 'jay'@'localhost';
+ALTER USER 'jay'@'localhost' IDENTIFIED BY 'P$55!23';
